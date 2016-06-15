1a2fa6bec54bf37d78c34d1bce523fc30f7ea96f
Chrisna Aing <chrisna@chrisna.org>
2016-06-15 14:24:38 -0700

    Initial Commit

---
+++ NetworkingKit/NetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingKit.h
+//  NetworkingKit
+//
+//  Created by Chrisna Aing on 6/15/16.
+//  Copyright © 2016 Chrisna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkingKit.
+FOUNDATION_EXPORT double NetworkingKitVersionNumber;
+
+//! Project version string for NetworkingKit.
+FOUNDATION_EXPORT const unsigned char NetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingKit/PublicHeader.h>
+
+
